c0db721eb8c16b2e40c1ee6f765da345974d74ab
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2024-03-22 09:32:26 -0400

    update version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='pl-notification',
-    version='0.0.2',
+    version='0.1.0',
     author='FNNDSC',
     author_email='dev@babyMRI.org',
     description='A ChRIS Plugin for notification through mail / Slack / Element',
